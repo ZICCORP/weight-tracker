c9963b11f9170e9b4e6ece45511c9721881fc9b5
ZICCORP <frankchuka250@gmail.com>
2020-11-26 02:18:30 +0100

    Added main app to installed apps
    Added a model class 'BodyWeight' and two field: 'weight' and 'date'
    Added test for the 'BodyWeight' model

--- main/models.py
+++ main/models.py
@@ -1,3 +1,5 @@
 from django.db import models
 
-# Create your models here.
+class BodyWeight(models.Model):
+    weight = models.IntegerField()
+    date = models.DateTimeField(auto_now_add=True,unique=True)
--- main/tests.py
+++ main/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from main.models import BodyWeight
 
-# Create your tests here.
+class TestModel(TestCase):
+    def setUp(self):
+        self.bweight=BodyWeight.objects.create(
+            weight=77
+        )
+    def test_weight_contains_correct_values(self):
+        self.assertEquals(self.bweight.weight,77)
+    
+    def test_weight_doesNotContain_correct_value(self):
+        self.assertNotEqual(self.bweight.weight,79)
