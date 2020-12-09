20ea22d191e2f83363ae2ef424e634af35039470
ZICCORP <frankchuka250@gmail.com>
2020-12-09 16:38:10 +0100

    Added design for the activity page and change my bar chart rendering engine from d3.js to convas.js

--- main/views.py
+++ main/views.py
@@ -26,20 +26,30 @@ class BweightView(LoginRequiredMixin,View):
         """
         calender = {1:'Jan',2:'Feb',3:'Mar',4:'Apr',5:'May',6:'Jun',7:'Jul',8:'Aug',9:'Sep',10:'Oct',11:'Nov',12:'Dec'}
         data = model.objects.filter(owner=self.request.user).values().order_by('date')
+        a= 0
         d= []
+       
         for i in data:
+            g={}
             for j in i:
                 if j=='date':
+                   
                     d_obj = i[j]
                     d_day = d_obj.day
                     d_month = calender[d_obj.month]
                     d_year = d_obj.year
-                    i['date'] =str(d_month)+' '+str(d_day)+', '+str(d_year)[:2]
-            d.append(i)
+                    g['label'] =str(d_month)+' '+str(d_day)+', '+str(d_year)
+                   
+                elif j=='weight':
+                    w_obj = i[j]
+                    g['y'] = w_obj
+           
+            d.append(g)
 
-        d.append({'weight':150,'date':'40'})
-        d.append({'weight':98,'date':'20'})
-        d.append({'weight':50,'date':'10'})
+        d.append({'label':'40','y':150})
+        d.append({'label':'20','y':98})
+        d.append({'label':10,'y':50})
+       
         return d
 
     def get(self,request,*args,**kwargs):
